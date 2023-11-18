8e12071c2c8f66364489f0fbfc3c1102d0d6dc39
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2023-11-18 02:38:57 -0300

    sets up typeorm database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {dirname} from 'node:path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: 'db',
+        port: 3306,
+        username: 'root',
+        password: configService.get<string>('MYSQL_ROOT_PASSWORD'),
+        database: 'api_base',
+        entities: [dirname('.') + '/**/entities/*.entity{.ts,.js}']
+      }),
+      inject: [ConfigService]
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
